db66f8e1737632f015b3d3ab9e4a0d514bc95da0
dev.osdk.jerry@dji.com <dev.osdk.jerry@dji.com>
2020-12-25 20:50:04 +0800

    NEW:update OSDK 4.1 doc
    Update OSDK 4.1 doc.

--- api-reference/search/functions_d.js
+++ api-reference/search/functions_d.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['obtainctrlauthority',['obtainCtrlAuthority',['../classDJI_1_1OSDK_1_1Control.html#aa7078dd77045eef97040a44602d02a4d',1,'DJI::OSDK::Control::obtainCtrlAuthority(VehicleCallBack callback=0, UserData userData=0)'],['../classDJI_1_1OSDK_1_1Control.html#a9a855cd3f59965ebd287685d7772ff1b',1,'DJI::OSDK::Control::obtainCtrlAuthority(int timeout)']]],
-  ['obtaindownloadrightsync',['obtainDownloadRightSync',['../classDJI_1_1OSDK_1_1CameraManager.html#a8022cb2ba3c24d9d97c27907f86b702a',1,'DJI::OSDK::CameraManager::obtainDownloadRightSync()'],['../classDJI_1_1OSDK_1_1CameraModule.html#ac758dd371b5a8b01e3fef6ef684640da',1,'DJI::OSDK::CameraModule::obtainDownloadRightSync()']]]
+  ['obtaindownloadrightsync',['obtainDownloadRightSync',['../classDJI_1_1OSDK_1_1CameraManager.html#a8022cb2ba3c24d9d97c27907f86b702a',1,'DJI::OSDK::CameraManager::obtainDownloadRightSync()'],['../classDJI_1_1OSDK_1_1CameraModule.html#ac758dd371b5a8b01e3fef6ef684640da',1,'DJI::OSDK::CameraModule::obtainDownloadRightSync()']]],
+  ['obtainjoystickctrlauthorityasync',['obtainJoystickCtrlAuthorityAsync',['../classDJI_1_1OSDK_1_1FlightController.html#a3f1f4c374c43db6b0109b92d4acd800c',1,'DJI::OSDK::FlightController']]],
+  ['obtainjoystickctrlauthoritysync',['obtainJoystickCtrlAuthoritySync',['../classDJI_1_1OSDK_1_1FlightController.html#ad3a5d76c2ad305cb0b99ea62de311f24',1,'DJI::OSDK::FlightController']]]
 ];
